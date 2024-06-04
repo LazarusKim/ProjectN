b1ec4e39684eb5f9ce6bf86460ed855ac1a65a06
LazarusKim <119262558+LazarusKim@users.noreply.github.com>
2024-06-04 23:10:58 +0900

    06.04
    Grid Button
    Nikke Illust

--- Assets/Scenes/Scripts/AimSelector.cs
+++ Assets/Scenes/Scripts/AimSelector.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,17 +6,47 @@ using UnityEngine.UI;
 
 public class AimSelector : MonoBehaviour
 {
-    [SerializeField] private Sprite[] crosshairs;
-    private int numCrossHairs = 0;
+    private string[] weaponTypesKeys;
+    [SerializeField] private Sprite[] crosshairsValues;
+    private Dictionary<string, Sprite> crossHairs;
+    
     private Image crosshair;
     private RectTransform crosshairRectTransform;
     private AimController _aimController;
+
+    private enum WeaponType
+    {
+        AR,
+        SMG,
+        SG,
+        SR,
+        RL,
+        MG
+    }
+
+    private void InitWeaponMap()
+    {
+        crossHairs = new Dictionary<string, Sprite>();
+        weaponTypesKeys = Enum.GetNames(typeof(WeaponType));   
+        
+        for(int i = 0; i < crosshairsValues.Length; i++)
+        {
+            crossHairs.Add(weaponTypesKeys[i], crosshairsValues[i]);
+            Debug.Log(weaponTypesKeys[i]);
+        }
+    }
+    void Awake()
+    {
+        InitWeaponMap();
+    }
+
+    
+
     void Start()
     {
         crosshair = GetComponentInParent<Image>();
         crosshairRectTransform = GetComponentInParent<RectTransform>();
         _aimController = crosshair.GetComponentInParent<AimController>();
-        //crosshair.sprite = crosshairs[numCrossHairs];
     }
 
     
@@ -24,21 +55,21 @@ public class AimSelector : MonoBehaviour
         // Test code
         // When the click nikke illust, crosshair gonna change
         // Set default transform when sprite changed
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            if (numCrossHairs < crosshairs.Length -1)
-            {
-                numCrossHairs++;
-
-            }
-            else
-            {
-                numCrossHairs = 0;
-            }
-
-            crosshairRectTransform.anchoredPosition = Vector2.zero;
-            crosshair.sprite = crosshairs[numCrossHairs];
-            _aimController.isOnDrag = false;
-        }
+        // if (Input.GetKeyDown(KeyCode.C))
+        // {
+        //     if (numCrossHairs < crosshairs.Length -1)
+        //     {
+        //         numCrossHairs++;
+        //
+        //     }
+        //     else
+        //     {
+        //         numCrossHairs = 0;
+        //     }
+        //
+        //     crosshairRectTransform.anchoredPosition = Vector2.zero;
+        //     crosshair.sprite = crosshairs[numCrossHairs];
+        //     _aimController.isOnDrag = false;
+        // }
     }
 }
